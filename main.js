ed246dfb55acff6b8cd47768ac82e7dbfa680c24
MariaCiril <149600889+MariaCiril@users.noreply.github.com>
2024-04-11 16:41:31 -0300

    8

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const dataAtual = new Date ();
 let dataNiver = prompt ("data do próximo niver :");
 dataNiver = new Date (dataNiver + "T23:59:59");
+let diasQueFaltam = Math.Floor((dataNiver-dataAtual) /86400000
